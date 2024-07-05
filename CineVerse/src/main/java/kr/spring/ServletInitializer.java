6cb9d47383977a1896f8c284d4e5ec55c1ac3e40
user <user@DESKTOP-LC52269>
2024-07-05 17:49:42 +0900

    2024/07/05

--- CineVerse/src/main/java/kr/spring/ServletInitializer.java
+++ CineVerse/src/main/java/kr/spring/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SineVerseApplication.class);
+		return application.sources(CineVerseApplication.class);
 	}
 
 }
--- CineVerse/src/test/java/kr/spring/CineVerseApplicationTests.java
+++ CineVerse/src/test/java/kr/spring/CineVerseApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SineVerseApplicationTests {
+class CineVerseApplicationTests {
 
 	@Test
 	void contextLoads() {
