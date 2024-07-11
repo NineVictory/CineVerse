759939c855aed74bd5934bd834301982d89039f0
minji0626 <jo010163@naver.com>
2024-07-11 14:48:53 +0900

    .

--- CineVerse/src/main/java/kr/spring/myPage/vo/MyPageVO.java
+++ CineVerse/src/main/java/kr/spring/myPage/vo/MyPageVO.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.Pattern;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import kr.spring.member.vo.CouponVO;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -63,7 +62,9 @@ public class MyPageVO {
 	private int coupon_sale;		// 할인 금액
 	private int coupon_where;		// 어디서 사용 가능한지 1:영화 2:벌스샵
 	private Date coupon_regdate;	// 쿠폰 발급일자
-	private Date coupon_enddate;
+	private Date coupon_enddate;	// 쿠폰 만료일자
+	
+	
 	// 비밀번호 변경시 현재 비밀번호를 저장하는 용도로 사용
 	@Pattern(regexp = "^[0-9a-zA-Z]{4,12}$")
 	private String now_passwd;	// 컬럼은 없지만 필요에 의해 생성해줌
