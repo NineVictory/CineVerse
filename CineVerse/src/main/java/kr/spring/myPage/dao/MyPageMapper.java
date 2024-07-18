7592f78e1e14691e50e7a5ad043856b5b62da2eb
boonmojae <pminny99@naver.com>
2024-07-18 15:55:34 +0900

    /

--- CineVerse/src/main/java/kr/spring/myPage/dao/MyPageMapper.java
+++ CineVerse/src/main/java/kr/spring/myPage/dao/MyPageMapper.java
@@ -19,7 +19,7 @@ import kr.spring.myPage.vo.MyPageVO;
 public interface MyPageMapper {
 	@Select("SELECT * FROM member JOIN member_detail  USING(mem_num) WHERE mem_num=#{mem_num}")
 	public MyPageVO selectMember(Long mem_num);//회원 정보 가져오기
-	@Select("SELECT COUNT(*) FROM member_coupon WHERE mem_num=#{mem_num}")
+	@Select("SELECT COUNT(*) FROM member_coupon WHERE mem_num=#{mem_num} AND coupon_use = 1")
 	public Integer selectMemberCoupon(Long mem_num);	// 회원 쿠폰 개수 구하기
 	public List<MyPageVO> selectMemCouponList(Map<String, Object> map);	// 회원 쿠폰 목록 불러오기
 	public Integer cBoardWriteListcnt(Map<String, Object> map);
